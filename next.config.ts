df10f7ec20cee9e59890962e7cbd984737032653
ihor <ihor@192.168.100.2>
2024-12-16 17:50:44 +0200

    removed assetpath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   basePath: '/2Track', 
-  assetPrefix: './',
   output: "export",
   eslint: {
     ignoreDuringBuilds: true, 
