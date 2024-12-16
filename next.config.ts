063271fb0daf44326e8a61ef12d9da86e62da07d
ihor <ihor@192.168.100.2>
2024-12-16 17:15:36 +0200

    fix assets

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   basePath: '/2Track', 
-  assetPrefix: '/2Track/',
+  assetPrefix: '/2Track',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true, 
   },
