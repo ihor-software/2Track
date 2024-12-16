538ad54b5c608af0105b2c30baa5e33912342a07
ihor <ihor@192.168.100.2>
2024-12-16 16:17:02 +0200

    revert deletion

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export', 
+  basePath: '/2Track', 
   eslint: {
     ignoreDuringBuilds: true, 
   },
